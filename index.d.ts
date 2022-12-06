bbda0af0c901b3082caece09c482a4f8b63da6dc
Audric Ackermann <audric@loki.network>
2022-12-06 15:51:10 +1100

    fix: update ts typings with latest infos

--- index.d.ts
+++ index.d.ts
@@ -2,9 +2,9 @@ declare module "session_util_wrapper" {
   export abstract class BaseConfigWrapper {
     public needsDump(): boolean;
     public needsPush(): boolean;
-    public push(): { toPush: Uint8Array; seqno: number };
+    public push(): { data: Uint8Array; seqno: number };
     public dump(): Uint8Array;
-    public confirmPushed(): void;
+    public confirmPushed(seqno: number): void;
     public merge(toMerge: Array<Uint8Array>): void;
   }
   export class UserConfigWrapper extends BaseConfigWrapper {
@@ -14,7 +14,7 @@ declare module "session_util_wrapper" {
     public getProfilePic(): {
       url: string | null;
       key: Uint8Array | null;
-    } | null;
+    };
     public setProfilePic(url: string | null, key: Uint8Array | null);
   }
 }
