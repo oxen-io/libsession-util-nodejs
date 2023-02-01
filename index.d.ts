1c8dfa3bf86d5e5b878595421c1a5829954642eb
Audric Ackermann <audric@loki.network>
2023-02-01 15:04:22 +1100

    add typing for wrapper functions

--- index.d.ts
+++ index.d.ts
@@ -1,49 +1,170 @@
 declare module 'session_util_wrapper' {
+
+  /**
+   * This is quite a complex setup, but so far I didn't find an easier way to describe what we want to do with the wrappers and have strong typings
+   *
+   * Essentially, each of the wrappers have 2 sides
+   *  - one side are calls made by the webworker directly to the wrapper
+   *  - the other side are calls made by the renderer to the webworker (which should forward to the wrapper)
+   *
+   * We cannot pass unserializable data between those two, so we need to have a serialazable way of calling one
+   * method of a wrapper with the required arguments.
+   * Those serializable data, are `UserConfigActionsType` or just any of the `*ActionsType`. They are defined with a tuple of what each methods accepts on which wrapper with which argument.
+   *
+   * Then, we need to define the side of what the worker can call directly on the wrapper & the calls to be made from the renderer to the webworker.
+   * To avoid too much duplication, I've created a third Type, which is reused in the other two.
+   * The `ContactsWrapper` or `UserConfigWrapper` is that third Type.
+   * The `*InsideWorker` is the part reusing that Type to enable the webworker to do its actions directly.
+   * The `*WrapperActionsCalls` is the part which can be called from the renderer to do actions through the webworker on the wrappers
+   *
+   *
+   *
+   * If at some point we get to make all of those type derived from `ContactsWrapper` or `UserConfigWrapper` it would be nice. Typescript is probably already able to do it, with some pain.
+   *
+   */
+
+  /**
+   *
+   * Utilities
+   *
+   */
+
+  type AsyncWrapper<T extends (...args: any) => any> = (
+    ...args: Parameters<T>
+  ) => Promise<ReturnType<T>>;
+
+
+
+  type MakeWrapperActionCalls<Type extends BaseConfigWrapper> = {
+    [Property in keyof Type]: AsyncWrapper<Type[Property]>
+  };
+
+
   export type ProfilePicture = {
     url: string | null;
     key: Uint8Array | null;
   };
 
-  export abstract class BaseConfigWrapper {
-    public needsDump(): boolean;
-    public needsPush(): boolean;
-    public push(): { data: Uint8Array; seqno: number };
-    public dump(): Uint8Array;
-    public confirmPushed(seqno: number): void;
-    public merge(toMerge: Array<Uint8Array>): number;
-    public storageNamespace(): number;
-    public encryptionDomain(): string;
-  }
+  export type PushConfigResult = { data: Uint8Array; seqno: number }
+
 
   type MakeActionCall<A extends BaseConfigWrapper, B extends string> = [
     B,
     ...Parameters<A[B]>
-  ]; // ReturnType<A[B]>,
+  ];
 
-  export type BaseConfigActions =
-    | MakeActionCall<BaseConfigWrapper, 'needsDump'>
-    | MakeActionCall<BaseConfigWrapper, 'needsPush'>
-    | MakeActionCall<BaseConfigWrapper, 'push'>
-    | MakeActionCall<BaseConfigWrapper, 'dump'>
-    | MakeActionCall<BaseConfigWrapper, 'confirmPushed'>
-    | MakeActionCall<BaseConfigWrapper, 'merge'>
-    | MakeActionCall<BaseConfigWrapper, 'storageNamespace'>
-    | MakeActionCall<BaseConfigWrapper, 'encryptionDomain'>;
+  /**
+   *
+   * Base Config wrapper logic
+   *
+   */
 
-  export class UserConfigWrapper extends BaseConfigWrapper {
-    constructor(secretKey: Uint8Array, dump: Uint8Array | null);
-    public getName(): string;
-    public setName(name: string);
-    public getProfilePicture(): ProfilePicture;
-    public setProfilePicture(url: string | null, key: Uint8Array | null);
+  type BaseConfigWrapper = {
+      needsDump: ()=> boolean;
+      needsPush: ()=> boolean;
+      push: ()=> PushConfigResult;
+      dump: ()=> Uint8Array;
+      confirmPushed: (seqno: number) => void;
+      merge: (toMerge: Array<Uint8Array>)=>  number;
+      storageNamespace: ()=>  number;
   }
 
+  type MakeActionsCall<A extends BaseConfigWrapper, B extends string> = [
+    B,
+    ...Parameters<A[B]>
+  ];
+  export type BaseConfigActions =
+  | MakeActionCall<BaseConfigWrapper, 'needsDump'>
+  | MakeActionCall<BaseConfigWrapper, 'needsPush'>
+  | MakeActionCall<BaseConfigWrapper, 'push'>
+  | MakeActionCall<BaseConfigWrapper, 'dump'>
+  | MakeActionCall<BaseConfigWrapper, 'confirmPushed'>
+  | MakeActionCall<BaseConfigWrapper, 'merge'>
+  | MakeActionCall<BaseConfigWrapper, 'storageNamespace'>;
+
+  export abstract class BaseConfigWrapperInsideWorker {
+    public needsDump: BaseConfigWrapper['needsDump']
+    public needsPush: BaseConfigWrapper['needsPush']
+    public push: BaseConfigWrapper['push']
+    public dump: BaseConfigWrapper['dump']
+    public confirmPushed: BaseConfigWrapper['confirmPushed']
+    public merge: BaseConfigWrapper['merge']
+    public storageNamespace: BaseConfigWrapper['storageNamespace']
+  }
+
+  export type BaseWrapperActionsCalls = MakeWrapperActionCalls<BaseConfigWrapper>;
+
+  /**
+   *
+   * User config wrapper logic
+   *
+   */
+
+
+  type UserConfigWrapper = BaseConfigWrapper & {
+    init: (secretKey: Uint8Array, dump: Uint8Array | null) => void;
+    getName:()=> string;
+    setName: (name: string)=> void;
+    getProfilePicture: ()=> ProfilePicture;
+    setProfilePicture: (url: string, key: Uint8Array) =>  void;
+  }
+
+
+  export type UserConfigWrapperActionsCalls = MakeWrapperActionCalls<UserConfigWrapper>;
+
+
+
+  /**
+   * To be used inside the web worker only (calls are synchronous and won't work asynchrously)
+   */
+  export class UserConfigWrapperInsideWorker extends BaseConfigWrapperInsideWorker {
+    constructor(secretKey: Uint8Array, dump: Uint8Array | null);
+    public getName: UserConfigWrapper['getName'];
+    public setName: UserConfigWrapper['setName'];
+    public getProfilePicture: UserConfigWrapper['getProfilePicture'];
+    public setProfilePicture: UserConfigWrapper['setProfilePicture'];
+  }
+
+
+
+  /**
+   * Those actions are used internally for the web worker communication.
+   * You should never need to import them in Session directly
+   * You will need to add an entry here if you add a new function
+   */
   export type UserConfigActionsType =
     | ['init', Uint8Array, Uint8Array | null]
     | MakeActionCall<UserConfigWrapper, 'getName'>
     | MakeActionCall<UserConfigWrapper, 'setName'>
     | MakeActionCall<UserConfigWrapper, 'getProfilePicture'>
     | MakeActionCall<UserConfigWrapper, 'setProfilePicture'>;
+
+
+  /**
+   *
+   * Contacts wrapper logic
+   *
+   */
+
+  type ContactsWrapper = BaseConfigWrapper & {
+    init: (secretKey: Uint8Array, dump: Uint8Array | null) => void;
+    get:(pubkeyHex: string)=> ContactInfo | null;
+    getOrCreate:(pubkeyHex: string)=>ContactInfo;
+    set:(contact: ContactInfo)=> void;
+    setName:(pubkeyHex: string, name: string)=> void;
+    setNickname:(pubkeyHex: string, nickname: string)=> void;
+    setApproved:(pubkeyHex: string, approved: boolean)=> void;
+    setApprovedMe:(pubkeyHex: string, approvedMe: boolean) => void;
+    setBlocked:(pubkeyHex: string, blocked: boolean)=> void;
+    setProfilePicture: (pubkeyHex: string, url: string, key: Uint8Array) => void;
+    getAll: () => Array<ContactInfo>;
+    erase: (pubkeyHex: string)=> void;
+  }
+
+
+  export type ContactsWrapperActionsCalls = MakeWrapperActionCalls<ContactsWrapper>;
+
+
 
   export type ContactInfo = {
     id: string;
@@ -55,40 +176,32 @@ declare module 'session_util_wrapper' {
     blocked?: boolean;
   };
 
-  export class ContactsConfigWrapper extends BaseConfigWrapper {
+  export class ContactsConfigWrapperInsideWorker extends BaseConfigWrapperInsideWorker {
     constructor(secretKey: Uint8Array, dump: Uint8Array | null);
-
-    public get(pubkeyHex: string): ContactInfo | null;
-    public getOrCreate(pubkeyHex: string): ContactInfo;
-    public set(contact: ContactInfo);
-
-    public setName(pubkeyHex: string, name: string);
-    public setNickname(pubkeyHex: string, nickname: string);
-    public setApproved(pubkeyHex: string, approved: boolean);
-    public setApprovedMe(pubkeyHex: string, approvedMe: boolean);
-    public setBlocked(pubkeyHex: string, blocked: boolean);
-    public setProfilePicture(
-      pubkeyHex: string,
-      url: string | null,
-      key: Uint8Array | null
-    );
-
-    public getAll(): Array<ContactInfo>;
-
-    public erase(pubkeyHex: string);
+    public get: ContactsWrapper['get'];
+    public getOrCreate: ContactsWrapper['getOrCreate'];
+    public set: ContactsWrapper['set'];
+    public setName: ContactsWrapper['setName'];
+    public setNickname: ContactsWrapper['setNickname'];
+    public setApproved: ContactsWrapper['setApproved'];
+    public setApprovedMe: ContactsWrapper['setApprovedMe'];
+    public setBlocked: ContactsWrapper['setBlocked'];
+    public setProfilePicture: ContactsWrapper['setProfilePicture'];
+    public getAll: ContactsWrapper['getAll'];
+    public erase: ContactsWrapper['erase'];
   }
 
   export type ContactsConfigActionsType =
     | ['init', Uint8Array, Uint8Array | null]
-    | MakeActionCall<ContactsConfigWrapper, 'get'>
-    | MakeActionCall<ContactsConfigWrapper, 'getOrCreate'>
-    | MakeActionCall<ContactsConfigWrapper, 'set'>
-    | MakeActionCall<ContactsConfigWrapper, 'setName'>
-    | MakeActionCall<ContactsConfigWrapper, 'setNickname'>
-    | MakeActionCall<ContactsConfigWrapper, 'setApproved'>
-    | MakeActionCall<ContactsConfigWrapper, 'setApprovedMe'>
-    | MakeActionCall<ContactsConfigWrapper, 'setBlocked'>
-    | MakeActionCall<ContactsConfigWrapper, 'setProfilePicture'>
-    | MakeActionCall<ContactsConfigWrapper, 'getAll'>
-    | MakeActionCall<ContactsConfigWrapper, 'erase'>;
+    | MakeActionCall<ContactsWrapper, 'get'>
+    | MakeActionCall<ContactsWrapper, 'getOrCreate'>
+    | MakeActionCall<ContactsWrapper, 'set'>
+    | MakeActionCall<ContactsWrapper, 'setName'>
+    | MakeActionCall<ContactsWrapper, 'setNickname'>
+    | MakeActionCall<ContactsWrapper, 'setApproved'>
+    | MakeActionCall<ContactsWrapper, 'setApprovedMe'>
+    | MakeActionCall<ContactsWrapper, 'setBlocked'>
+    | MakeActionCall<ContactsWrapper, 'setProfilePicture'>
+    | MakeActionCall<ContactsWrapper, 'getAll'>
+    | MakeActionCall<ContactsWrapper, 'erase'>;
 }
