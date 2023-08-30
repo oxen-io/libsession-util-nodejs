3a78469b9c5e593d52c1869b39e9ba1afb50de37
Audric Ackermann <audric@loki.network>
2023-08-30 11:20:00 +1000

    chore: break up index.d.ts into smaller files

---
+++ user/contacts.d.ts
@@ -0,0 +1,55 @@
+/**
+ *
+ * Contacts wrapper logic
+ *
+ */
+declare module 'libsession_util_nodejs' {
+  type ContactsWrapper = BaseConfigWrapper & {
+    init: (secretKey: Uint8Array, dump: Uint8Array | null) => void;
+    get: (pubkeyHex: string) => ContactInfo | null;
+    set: (contact: ContactInfoSet) => void;
+    getAll: () => Array<ContactInfo>;
+    erase: (pubkeyHex: string) => void;
+  };
+
+  export type ContactsWrapperActionsCalls = MakeWrapperActionCalls<ContactsWrapper>;
+
+  type ContactInfoShared = {
+    id: string;
+    name?: string;
+    nickname?: string;
+    profilePicture?: ProfilePicture;
+    priority: number; // -1 means hidden, 0 means normal, > 1 means pinned
+    createdAtSeconds: number; // can only be set the first time a contact is created, a new change won't overide the value in the wrapper.
+
+    // expirationMode: 'off' | 'disappearAfterRead' | 'disappearAfterSend'; // the same as defined in the disappearingBranch
+    // expirationTimerSeconds: number;
+  };
+
+  export type ContactInfoSet = ContactInfoShared & {
+    approved?: boolean;
+    approvedMe?: boolean;
+    blocked?: boolean;
+  };
+
+  export type ContactInfo = ContactInfoShared & {
+    approved: boolean;
+    approvedMe: boolean;
+    blocked: boolean;
+  };
+
+  export class ContactsConfigWrapperNode extends BaseConfigWrapperNode {
+    constructor(secretKey: Uint8Array, dump: Uint8Array | null);
+    public get: ContactsWrapper['get'];
+    public set: ContactsWrapper['set'];
+    public getAll: ContactsWrapper['getAll'];
+    public erase: ContactsWrapper['erase'];
+  }
+
+  export type ContactsConfigActionsType =
+    | ['init', Uint8Array, Uint8Array | null]
+    | MakeActionCall<ContactsWrapper, 'get'>
+    | MakeActionCall<ContactsWrapper, 'set'>
+    | MakeActionCall<ContactsWrapper, 'getAll'>
+    | MakeActionCall<ContactsWrapper, 'erase'>;
+}
---
+++ user/convovolatile.d.ts
@@ -0,0 +1,75 @@
+declare module 'libsession_util_nodejs' {
+  export type ConvoVolatileType = '1o1' | UserGroupsType;
+
+  export type BaseConvoInfoVolatile = {
+    lastRead: number; // defaults to 0, unixTimestamp in ms
+    unread: boolean; // defaults to false
+  };
+
+  type ConvoInfoVolatile1o1 = BaseConvoInfoVolatile & { pubkeyHex: string };
+  type ConvoInfoVolatileLegacyGroup = BaseConvoInfoVolatile & { pubkeyHex: string };
+  type ConvoInfoVolatileCommunity = BaseConvoInfoVolatile & CommunityDetails;
+
+  // type ConvoInfoVolatileCommunity = BaseConvoInfoVolatile & { pubkeyHex: string }; // we need a `set` with the full url but maybe not for the `get`
+
+  type ConvoInfoVolatileWrapper = BaseConfigWrapper & {
+    init: (secretKey: Uint8Array, dump: Uint8Array | null) => void;
+
+    // 1o1 related methods
+    get1o1: (pubkeyHex: string) => ConvoInfoVolatile1o1 | null;
+    getAll1o1: () => Array<ConvoInfoVolatile1o1>;
+    set1o1: (pubkeyHex: string, lastRead: number, unread: boolean) => void;
+    erase1o1: (pubkeyHex: string) => void;
+
+    // legacy group related methods
+    getLegacyGroup: (pubkeyHex: string) => ConvoInfoVolatileLegacyGroup | null;
+    getAllLegacyGroups: () => Array<ConvoInfoVolatileLegacyGroup>;
+    setLegacyGroup: (pubkeyHex: string, lastRead: number, unread: boolean) => void;
+    eraseLegacyGroup: (pubkeyHex: string) => boolean;
+
+    // communities related methods
+    getCommunity: (communityFullUrl: string) => ConvoInfoVolatileCommunity | null; // pubkey not required
+    getAllCommunities: () => Array<ConvoInfoVolatileCommunity>;
+    setCommunityByFullUrl: (fullUrlWithPubkey: string, lastRead: number, unread: boolean) => void;
+    eraseCommunityByFullUrl: (fullUrlWithOrWithoutPubkey: string) => void;
+  };
+
+  export type ConvoInfoVolatileWrapperActionsCalls =
+    MakeWrapperActionCalls<ConvoInfoVolatileWrapper>;
+
+  export class ConvoInfoVolatileWrapperNode extends BaseConfigWrapperNode {
+    constructor(secretKey: Uint8Array, dump: Uint8Array | null);
+    // 1o1 related methods
+    public get1o1: ConvoInfoVolatileWrapper['get1o1'];
+    public getAll1o1: ConvoInfoVolatileWrapper['getAll1o1'];
+    public set1o1: ConvoInfoVolatileWrapper['set1o1'];
+    public erase1o1: ConvoInfoVolatileWrapper['eraseLegacyGroup'];
+
+    // legacy-groups related methods
+    public getLegacyGroup: ConvoInfoVolatileWrapper['getLegacyGroup'];
+    public getAllLegacyGroups: ConvoInfoVolatileWrapper['getAllLegacyGroups'];
+    public setLegacyGroup: ConvoInfoVolatileWrapper['setLegacyGroup'];
+    public eraseLegacyGroup: ConvoInfoVolatileWrapper['eraseLegacyGroup'];
+
+    // communities related methods
+    public getCommunity: ConvoInfoVolatileWrapper['getCommunity'];
+    public setCommunityByFullUrl: ConvoInfoVolatileWrapper['setCommunityByFullUrl'];
+    public getAllCommunities: ConvoInfoVolatileWrapper['getAllCommunities'];
+    public eraseCommunityByFullUrl: ConvoInfoVolatileWrapper['eraseCommunityByFullUrl'];
+  }
+
+  export type ConvoInfoVolatileConfigActionsType =
+    | ['init', Uint8Array, Uint8Array | null]
+    | MakeActionCall<ConvoInfoVolatileWrapper, 'get1o1'>
+    | MakeActionCall<ConvoInfoVolatileWrapper, 'getAll1o1'>
+    | MakeActionCall<ConvoInfoVolatileWrapper, 'set1o1'>
+    | MakeActionCall<ConvoInfoVolatileWrapper, 'erase1o1'>
+    | MakeActionCall<ConvoInfoVolatileWrapper, 'getLegacyGroup'>
+    | MakeActionCall<ConvoInfoVolatileWrapper, 'getAllLegacyGroups'>
+    | MakeActionCall<ConvoInfoVolatileWrapper, 'setLegacyGroup'>
+    | MakeActionCall<ConvoInfoVolatileWrapper, 'eraseLegacyGroup'>
+    | MakeActionCall<ConvoInfoVolatileWrapper, 'getCommunity'>
+    | MakeActionCall<ConvoInfoVolatileWrapper, 'setCommunityByFullUrl'>
+    | MakeActionCall<ConvoInfoVolatileWrapper, 'getAllCommunities'>
+    | MakeActionCall<ConvoInfoVolatileWrapper, 'eraseCommunityByFullUrl'>;
+}
