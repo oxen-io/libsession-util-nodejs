835dc91f6bc3a2068bb1aa8723811330795fd896
Audric Ackermann <audric@loki.network>
2024-03-26 09:23:01 +1100

    feat: add multi encrypt/decrypt simple wrapped functions

--- src/addon.cpp
+++ src/addon.cpp
@@ -3,6 +3,7 @@
 #include "contacts_config.hpp"
 #include "convo_info_volatile_config.hpp"
 #include "groups/meta_group_wrapper.hpp"
+#include "multi_encrypt/multi_encrypt.hpp"
 #include "user_config.hpp"
 #include "user_groups_config.hpp"
 
@@ -16,6 +17,9 @@ Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
     ContactsConfigWrapper::Init(env, exports);
     UserGroupsWrapper::Init(env, exports);
     ConvoInfoVolatileWrapper::Init(env, exports);
+
+    // Fully static wrappers init
+    MultiEncryptWrapper::Init(env, exports);
 
     return exports;
 }
---
+++ types/multi_encrypt/index.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="../shared.d.ts" />
+/// <reference path="./multi_encrypt.d.ts" />
